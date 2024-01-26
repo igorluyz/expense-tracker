f3770094b17dfacce268094bbfac4477f8ad4d1a
Igor Luiz <igorprofessionalcontact@gmail.com>
2024-01-26 13:06:22 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+    { date: new Date(2023, 12, 5), category: 'food', title: 'McDonalds', value: 32.12  },
+    { date: new Date(2023, 12, 5), category: 'food', title: 'BurgerKing', value: 40.12  },
+    { date: new Date(2023, 12, 7), category: 'rent', title: 'AluguelApt', value: 2500  },
+    { date: new Date(2023, 11, 18), category: 'salary', title: 'Salário Trader', value: 20000  },
+];
---
+++ src/helpers/dateFilter.ts
@@ -0,0 +1,44 @@
+// Manipulação de Datas
+
+import { Item } from "../types/Item";
+
+export const getCurrentMonth = () => {
+    let now = new Date();
+    return `${now.getFullYear()}-${now.getMonth()+1}`
+}
+
+export const filterListByMonth = (list: Item[], date: string): Item[] => {
+    let newList: Item[] = [];
+    let [year, month] = date.split('-');
+
+    for(let i in list){
+        if(
+            list[i].date.getFullYear() === parseInt(year) &&
+            (list[i].date.getMonth() + 1) === parseInt(month)
+        ) {
+            newList.push(list[i]);
+        }
+    }
+
+    return newList;
+}
+
+
+export const formatDate = (date: Date): string => {
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+
+    return `${day.toString().padStart(2, "0")}/${month.toString().padStart(2, "0")}/${year}`
+}
+
+export const formatCurrentMonth = (currentMonth: string): string => {
+    let [year, month] = currentMonth.split('-');
+    let months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+    return `${months[parseInt(month) - 1]} de ${year}`;
+}
+
+export const newDateAdjusted = (dateField: string) => {
+    let [year, month, day] = dateField.split('-')
+    return new Date(parseInt(year), parseInt(month) - 1, parseInt(day))
+  }
